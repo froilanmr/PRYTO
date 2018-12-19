717f7643a1a10d9a1ebc3103f9ca448cc6c9fa30
Froilan Moya Robles <froilanmoyarobles@gmail.com>
2018-12-19 15:32:58 -0600

    conexion

--- APP MUNI LIMON/APP MUNI LIMON/MenuActividades.aspx.cs
+++ APP MUNI LIMON/APP MUNI LIMON/MenuActividades.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +11,41 @@ public partial class Home : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "municipalidad.database.windows.net";
+            builder.UserID = "adminLimon";
+            builder.Password = "Actividades1.";
+            builder.InitialCatalog = "Municipalidad";
 
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("=========================================\n");
+
+                connection.Open();
+                StringBuilder sb = new StringBuilder();
+                sb.Append("SELECT *");
+                sb.Append("FROM Usuarios");
+                String sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                        }
+                    }
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        Console.ReadLine();
     }
 }
